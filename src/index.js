d61331fdcf9e4e377d9bc8043849af60dac9978d
Miguel Nazario <mnazario35@gmail.com>
2022-02-09 10:27:24 -0500

    commit tree add45befc5a9213d7abfa275e26c6a789583ed72

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+        // console.log(input.value);
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const title = document.getElementById("detail-title");
+            const summary = document.getElementById("detail-summary");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+
+        })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
